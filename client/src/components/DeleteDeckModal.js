238a270ce28b59122c0427613f17ffc37f985d07
jshafto <jpshafto@gmail.com>
2020-10-02 20:24:36 -0400

    card deletion is complete

--- client/src/components/DeleteDeckModal.js
+++ client/src/components/DeleteDeckModal.js
@@ -1,62 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams, useHistory } from 'react-router-dom';
 
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton'
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import Autocomplete from '@material-ui/lab/Autocomplete'
-import Switch from '@material-ui/core/Switch'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
 
 import { closeModal } from '../store/ui'
-import { createDeckThunk, clearDeck, deleteDeckThunk } from '../store/decks'
-import { loadCategoriesThunk, clearCategories } from '../store/categories';
+import { deleteDeckThunk } from '../store/decks'
+
 
 
 
 const DeleteDeckModal = () => {
-
   const open = useSelector(state => state.ui.modal === 'deleteDeckModal');
   const dispatch = useDispatch();
-
   const activeDeck = useSelector(state => state.entities.decks.activeDeck);
-
-
   const handleClose = () => {
     dispatch(closeModal());
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch(deleteDeckThunk(activeDeck.id ));
+    dispatch(deleteDeckThunk(activeDeck.id));
     dispatch(closeModal());
   };
 
   return (
     <div>
       <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" fullWidth={true}>
-        <DialogActions>
-          <IconButton onClick={handleClose}>
-            <CloseIcon />
-          </IconButton>
-        </DialogActions>
+
+        <IconButton
+          style={{
+            position: 'absolute',
+            right: 2,
+            top: 2,
+          }}
+          onClick={handleClose}>
+          <CloseIcon />
+        </IconButton>
+        <DialogTitle>
+          <Typography style={{ paddingRight: 20 }} variant="h6">Are you sure you want to delete your deck?</Typography>
+        </DialogTitle>
         <DialogContent>
-          <Typography variant="h5">Are you sure you want to delete your deck?</Typography>
           <form onSubmit={handleSubmit}>
-          <Button onClick={handleClose} color="secondary">
-              Cancel
+            <Grid container justify="flex-end">
+              <Button onClick={handleClose} color="primary">
+                Cancel
             </Button>
-            <Button type="submit" color="primary">
-              Delete deck
+              <Button type="submit" color="secondary">
+                Delete deck
             </Button>
+            </Grid>
           </form>
         </DialogContent>
       </Dialog>
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const routes = ['users','session', 'decks', 'categories'];
+const routes = ['users','session', 'decks', 'categories', 'cards'];
 
 for (let route of routes) {
   router.use(`/${route}`, require(`./${route}`));
