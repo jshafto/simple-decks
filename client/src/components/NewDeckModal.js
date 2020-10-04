fe242f5d0ba1823dda2eb4a03fd95ed299a5d812
jshafto <jpshafto@gmail.com>
2020-10-04 00:43:18 -0400

    finished dealing with all manageable warnings, cleaned up ui issues

--- client/src/components/NewDeckModal.js
+++ client/src/components/NewDeckModal.js
@@ -8,8 +8,6 @@ import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import CloseIcon from '@material-ui/icons/Close';
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
